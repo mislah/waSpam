8093e411eaa47e31b996104c40075077f6142fe5
Mislah <git@mislah.com>
2021-09-14 18:37:37 +0530

    Prevent accidental spamming
    Contact protection has been added to secure spamming. The user can now
    explicitly specify a contact to prevent accidentally spamming others.

--- source.js
+++ source.js
@@ -1,9 +1,14 @@
-function send(message, count = 1) {
+function send(message, count = 1, contact = '') {
+    if (contact) {
+        var select = new MouseEvent('mousedown', { bubbles: true, cancelable: true });
+        document.querySelector('[title="' + contact + '"]').dispatchEvent(select);
+    }
     var msgBox = document.querySelectorAll("[contenteditable='true']")[1];
-    var type = new UIEvent('input', {bubbles: true, cancelable: true, view: window});
+    var type = new UIEvent('input', {bubbles: true, cancelable: true, view: window})
+    sende = new MouseEvent('click', {bubbles: true, cancelable: true, view: window});
     while(count--) {
         msgBox.innerHTML = message;
         msgBox.dispatchEvent(type);
-        document.querySelector('span[data-icon="send"]').click();
+        document.querySelector('span[data-icon="send"]').dispatchEvent(sende);
     }
 }
