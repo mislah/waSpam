c0e88e68ee007e6b107ab06ca39bc7a533e75cce
Mislah <git@mislah.com>
2021-09-24 17:16:00 +0530

    Prevent freezing and enable early termination
    Previously, the web app was prone to freezing due to the simultaneous
    firing of all events, which caused excessive load. Moreover, it was not
    possible to stop the process until the message queue was empty. To
    address these issues, an interval has been introduced between each
    event dispatch. This allows the termination of the firing process at
    any point. A new function, stop(), has been implemented to facilitate
    this functionality.

--- source.js
+++ source.js
@@ -1,9 +1,15 @@
 function send(message, count = 1) {
     var msgBox = document.querySelectorAll("[contenteditable='true']")[1];
     var type = new UIEvent('input', {bubbles: true, cancelable: true, view: window});
-    while(count--) {
-        msgBox.innerHTML = message;
-        msgBox.dispatchEvent(type);
-        document.querySelector('span[data-icon="send"]').click();
-    }
+    interval = setInterval(()=>{
+            msgBox.innerHTML = message;
+            msgBox.dispatchEvent(type);
+            document.querySelector('span[data-icon="send"]').click();
+            if(!--count){
+                clearInterval(interval);
+            }
+        },100);
+}
+function stop(){
+    clearInterval(interval);
 }
