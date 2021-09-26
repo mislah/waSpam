2b1b9bbe5bf9d4a4d61321ab812fcdd398f44a52
Mislah <git@mislah.com>
2021-09-26 18:15:23 +0530

    Refactor and fix undefined behaviour causing bug
    A bug that caused undefined behavior when the input ended with "#0#" has
    been successfully fixed. Additionally, the code has been refactored to
    enhance readability.

--- source.js
+++ source.js
@@ -1,15 +1,16 @@
 // Repeat message n if #n# is at the end of the string
 document.addEventListener('click', () => {
-    document.querySelectorAll("[contenteditable='true']")[1].addEventListener("keydown", event => {
+    var msgBox = document.querySelectorAll("[contenteditable='true']")[1];
+    msgBox.addEventListener("keydown", event => {
         if(event.keyCode == 13){
-            var msgBox = document.querySelectorAll("[contenteditable='true']")[1];
             msgBox.innerHTML = msgBox.innerHTML.replace('<br>','');
-            if(msgBox.innerHTML.search(/(#\d+#$)/)!=-1){
+            if(msgBox.innerHTML.search(/#\d+#$/)!=-1){
                 event.preventDefault();
                 const type = new UIEvent('input', {bubbles: true, cancelable: true, view: window});
                 const enter = new KeyboardEvent('keydown', {keyCode: 13, bubbles: true, view: window});
-                var count = parseInt((msgBox.innerHTML.split(/(#\d+#$)/)[1]).slice(1,-1));
-                var message = msgBox.innerHTML.replace(/#\d+#$/,'');
+                var [message, count] = msgBox.innerHTML.split(/#(\d+)#$/);
+                msgBox.innerHTML = '';
+                count = parseInt(count);
                 while(count--) {
                     msgBox.innerHTML = message;
                     msgBox.dispatchEvent(type);
