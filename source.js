3cc61f793e82eb016be06d1d04209d0bb31ae0b0
Mislah <git@mislah.com>
2023-07-08 20:33:35 +0530

    Resolve critical issue with non-functioning of bot
    Due to recent updates in the Whatsapp web app, the bot was not
    functioning properly. This commit resolves the issue and restores
    the bot's functionality.

--- source.js
+++ source.js
@@ -1,22 +1,36 @@
 // Repeat message n if #n# is at the end of the string
 document.addEventListener('click', () => {
-    var msgBox = document.querySelectorAll("[contenteditable='true']")[1];
-    msgBox.addEventListener("keydown", event => {
-        if(event.keyCode == 13){
-            msgBox.innerHTML = msgBox.innerHTML.replace('<br>','');
-            if(msgBox.innerHTML.search(/#\d+#$/)!=-1){
-                event.preventDefault();
-                const type = new UIEvent('input', {bubbles: true, cancelable: true, view: window});
-                const enter = new KeyboardEvent('keydown', {keyCode: 13, bubbles: true, view: window});
-                var [message, count] = msgBox.innerHTML.split(/#(\d+)#$/);
-                msgBox.innerHTML = '';
-                count = parseInt(count);
-                while(count--) {
-                    msgBox.innerHTML = message;
-                    msgBox.dispatchEvent(type);
-                    msgBox.dispatchEvent(enter);
+    let msg = '';
+    let msgBox = document.querySelectorAll("[contenteditable='true']")[1];
+    msgBox.addEventListener("keyup", event => {
+        if (event.keyCode == 13 && msg.search(/#\d+#$/) != -1) {
+            let [message, count] = msg.split(/#(\d+)#$/);
+            count = parseInt(count);
+            (async () => {
+                while (count--) {
+                    await new Promise((resolve) => {
+                        msgBox.dispatchEvent(new InputEvent("beforeinput", {
+                            inputType: "insertText",
+                            data: message,
+                            bubbles: true,
+                            cancelable: true
+                        }));
+                        resolve();
+                    });
+                    msgBox.dispatchEvent(
+                        new KeyboardEvent('keydown', {
+                            key: "Enter",
+                            code: "Enter",
+                            which: 13,
+                            keyCode: 13,
+                            bubbles: true,
+                            view: window
+                        })
+                    )
+                    await new Promise(resolve => setTimeout(resolve, 10));
                 }
-            }
+            })();
         }
+        msg = msgBox.innerText;
     });
 });
